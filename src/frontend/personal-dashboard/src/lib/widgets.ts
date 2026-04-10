96e26108dc1d5f184b6fcad468835f51cf934441
KuechenMuesli <kuechenmuesli@gmail.com>
2026-04-10 16:10:12 +0200

    changed sizes

--- src/frontend/personal-dashboard/src/lib/widgets.ts
+++ src/frontend/personal-dashboard/src/lib/widgets.ts
@@ -9,12 +9,12 @@ import TimerStopwatch from "$lib/widgets/TimerStopwatch.svelte";
 
 
 export const widgets  = {
-	searchbar: { component: Searchbar, defaultSize: { width: 3, height: 2 } },
-	favorites: { component: Favorites, defaultSize: { width: 3, height: 4 } },
-	note:			 { component: Note, defaultSize: { width: 1, height: 5 }},
+	searchbar: { component: Searchbar, defaultSize: { width: 2, height: 2 } },
+	favorites: { component: Favorites, defaultSize: { width: 2, height: 4 } },
+	note:			 { component: Note, defaultSize: { width: 1, height: 4 }},
 	parcel:    { component: Parcel, defaultSize: { width: 1, height: 5 } },
 	trmnl:     { component: Trmnl, defaultSize: { width: 3, height: 7 } },
 	clockWeatherDate: { component: ClockWeatherDate, defaultSize: { width: 2, height: 1 } },
 	embed: { component: Embed, defaultSize: { width: 3, height: 7 } },
-	TimerStopwatch: { component: TimerStopwatch, defaultSize: { width: 4, height: 4 } },
+	TimerStopwatch: { component: TimerStopwatch, defaultSize: { width: 1, height: 3 } },
 }
